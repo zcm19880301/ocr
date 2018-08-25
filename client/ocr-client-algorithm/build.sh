35e3efc0d233461f3e76a0619975ffd0f097e374
wangpeihu <wangpeihu@163.com>
2018-08-25 16:21:53 +0800

    clean file

--- client/ocr-client-algorithm/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-curl -X POST http://127.0.0.1:11111/shutdown;
-git pull;
-mvn clean package -DskipTests=true;
-nohup java -jar -spring.profiles.active=prod target/kpi-0.0.1-SNAPSHOT.jar &
